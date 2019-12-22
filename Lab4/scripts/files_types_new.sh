251d4ba0837435a4de5d9121be3ea2de621a564a
Даниил <55439542+VeHH-GReT-H04@users.noreply.github.com>
2019-12-22 17:42:02 +0300

    3.4 - задание 1.1 но каталог задается как аргумент
    ; всегда отделяйте пользовательские данные от аргументов команды символами --"

---
+++ Lab4/scripts/files_types_new.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Каталоги"
+ls -al -- "$1" | grep ^d
+echo "Обычыные файлы"
+ls -al -- "$1" | grep ^-
+echo "Символьные ссылки"
+ls -al -- "$1" | grep ^1
+echo "Символьные устройства"
+ls -al -- "$1" | grep ^c
+echo "Блочные устройства"
+ls -al -- "$1" | grep ^b
